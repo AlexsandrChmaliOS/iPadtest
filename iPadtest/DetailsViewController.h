4d3716a9e0b9794dcf59ba4fb4473fa30fee0984
Sanya <sanychmal@rambler.ru>
2012-10-23 11:32:41 +0400

    Revert "Revert "GitHub for Mac: Throw-away commit.""
    This reverts commit 4b4760630b43b42ad27cdd46e91cd512a5dbfab6.

---
+++ iPadtest/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  iPadtest
+//
+//  Created by Sanya on 22.10.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
---
+++ iPadtest/TableViewController.h
@@ -0,0 +1,17 @@
+//
+//  TableViewController.h
+//  iPadtest
+//
+//  Created by Sanya on 22.10.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+{
+    NSMutableData* JsonData;
+    NSMutableArray* json ;
+    NSMutableArray* name ;
+}
+@end
