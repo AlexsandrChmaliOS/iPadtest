8bd5432893dfb8504566810c487083256757c604
Sanya <sanychmal@rambler.ru>
2012-10-23 11:22:35 +0400

    GitHub for Mac: Throw-away commit.

---
+++ iPadtest/DetailsViewController.m
@@ -0,0 +1,44 @@
+//
+//  DetailsViewController.m
+//  iPadtest
+//
+//  Created by Sanya on 22.10.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
